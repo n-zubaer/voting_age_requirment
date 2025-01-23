e3c3b0648f726ebe17b610c9215b0009bb670448
n-zubaer <nafizzubaer95@gmail.com>
2025-01-23 21:15:20 +0600

    vooting

---
+++ main.js
@@ -0,0 +1,14 @@
+// creating a voting requirment ages system
+
+let userInput = prompt("Enter Your Age");
+let age = +userInput;
+
+if (age >= 0 && age <= 17) {
+  alert("Not allowed for voting");
+} else if (age >= 18 && age <= 75) {
+  alert("Allowed for votting");
+} else if (age >= 76 && age <= 100) {
+  alert("Expired your votting ages");
+} else {
+  alert("Not accepted ");
+}
